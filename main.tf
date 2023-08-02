7bcd6a2cdec532426b3f75e63e4fa378a6c6cd33
patchigolla santhosh <santhosh.ramana96@gmail.com>
2023-08-02 16:37:40 +0530

    sdc

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 module "docdb" {
-  source                 = "./vendor/modules/docdb"
-  ENV                    = var.ENV
-  DOCDB_PORT_NUMBER      = var.DOCDB_PORT_NUMBER
-  DOCDB_INSTANCE_TYPE    = var.DOCDB_INSTANCE_TYPE 
-  DOCDB_INSTANCE_COUNT   = var.DOCDB_INSTANCE_COUNT
+  source                  = "./vendor/modules/docdb"
+  ENV                     = var.ENV
+  MYSQL_PORT_NUMBER       = var.MYSQL_PORT_NUMBER
+  MYSQL_STORAGE           = var.MYSQL_STORAGE
+  MYSQL_ENGINE_VERSION    = var.MYSQL_ENGINE_VERSION
+  MYSQL_INSTANCE_TYPE     = var.MYSQL_INSTANCE_TYPE
 }
 
 module "redis" {
